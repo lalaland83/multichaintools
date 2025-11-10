ec604821c8357ebc8ac4b1a869aae3c5cbd21e32
lalaland83 <156681656+lalaland83@users.noreply.github.com>
2025-11-10 12:19:57 +0700

    Update navigation.js

--- frontend/navigation.js
+++ frontend/navigation.js
@@ -5,10 +5,13 @@ function showPage(page) {
     // Die gewählte Seite anzeigen
     document.getElementById(page + "Page").style.display = "block";
 
+    
     updateWalletUI(); // UI nach Seitenwechsel aktualisieren
 
+    
     // Falls die "Blockchain Stats"-Seite geöffnet wird, Buttons generieren
     if (page === "blockchain_stats" && connectedWalletAddress) {
         setupBlockchainStats();
     }
 }
+
